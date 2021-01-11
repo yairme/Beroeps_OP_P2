25e28063d25ebd74d1f323e3630cdc0246abf83e
yairme <yairstward2013@gmail.com>
2021-01-11 16:15:28 +0100

    organized

--- WebPage_BP/WebXR/app.js
+++ WebPage_BP/WebXR/app.js
@@ -9,20 +9,16 @@ minecraftModel.setScale(0.1, 0.1, 0.1);
 minecraftModel.setRotation(0, -36.95, 0);
 
 // create a model
-const cuberoom = new Model('assets/models/cube-room/cube-room.gltf');
+const cuberoom = new Model('assets/Map_en_objects/Map/World.obj');
 cuberoom.setPosition(0, 0, -5.21484);
 cuberoom.setScale(1, 1.2, 1);
 cuberoom.setRotation(0, -36.95, 0);
 
 // create a primitive (sphere)
-const sphere = new Sphere();
-sphere.setColor('green');
-sphere.setRadius(0.5);
-sphere.setPosition(-1.63294, 0.70642,  -5.26931);
-sphere.setScale(2, 2, 2);
+
 
 // create some text
-const testText = new Text('Mediacollege Amsterdam WebXR');
+const testText = new Text('');
 testText.setPosition(-5.78309, 3.69223, -5);
 testText.setFontsize(10);
 testText.setScale(1.76, 2.28, 2.71);
@@ -36,7 +32,7 @@ const image2 = new XRImage('assets/images/Minecraft-2.jpg');
 image2.setPosition(1.57918, 2.45839, -5);
 
 // create a 360 image
-const image360 = new Sky('assets/images/BackGround.jpg');
+const image360 = new Sky('assets/Images/Daylight_Box_0.png');
 
 const spotLight = new SpotLight();
 spotLight.setPosition(-3.39382, 4.72931, 1.37905);
@@ -46,5 +42,4 @@ spotLight.setTarget(minecraftModel);
 minecraftModel.addEventListener('click', () => console.log('je klikt op mij'));
 
 // Event's text
-minecraftModel.addEventListener('mouseenter', () =>
-    document.getElementById('extraText').innerHTML = "Test pop up.");
+minecraftModel.addEventListener('mouseenter', () => document.getElementById('extraText').innerHTML = "Test pop up.");
