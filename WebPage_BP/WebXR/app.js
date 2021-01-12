fb39ae645d68f334dbae2087f879ae700ccc6dfc
yairme <yairstward2013@gmail.com>
2021-01-12 18:54:40 +0100

    Position R

--- WebPage_BP/WebXR/app.js
+++ WebPage_BP/WebXR/app.js
@@ -4,32 +4,31 @@ controls.enableMovement();
 
 //create a model
 const cuberoom = new Model("assets/Map_en_objects/Map/World.gltf");
-cuberoom.setPosition(-2.141, 0.000, 2.369);
+cuberoom.setPosition(-3.556, 0.000, 5.800);
 cuberoom.setScale(1, 1.2, 1);
-cuberoom.setRotation(0, -36.95, 0);
+cuberoom.setRotation(0.000, 158.949, 0.000);
 
 //create a model
 const melkMeisje = new Model('assets/vermeer/export_museumprops.babylon.gltf');
-melkMeisje.setPosition(-0.293, -0.05, -2.57);
+melkMeisje.setPosition(-1.523, -0.050, -2.570);
 melkMeisje.setScale(100, 100, 100);
 melkMeisje.setRotation(0, 45.45, 0);
 
-
 //Painting model
 const image_1 = new XRImage('assets/Images/Image_1.jpg');
-image_1.setPosition(-3.367, 1.112, 8.679);
+image_1.setPosition(-3.627, 2.127, 1.043);
 image_1.setScale(1, 1.2, 1);
 image_1.setRotation(0, -36.95, 0);
 
 //Painting model
 const image_2 = new XRImage('assets/Images/Image_2.jpg');
-image_2.setPosition(-5.672, 1.023, 7.405);
+image_2.setPosition(-3.627, 2.127, -0.417);
 image_2.setScale(1, 1.2, 1);
 image_2.setRotation(0, -36.95, 0);
 
 //Painting model
 const image_3 = new XRImage('assets/Images/Image_3.jpg');
-image_3.setPosition(-7.577, 1.069, 5.912);
+image_3.setPosition(-3.627, 2.127, -1.484);
 image_3.setScale(1, 1.2, 1);
 image_3.setRotation(0, -36.95, 0);
 
@@ -44,7 +43,7 @@ const image360 = new Sky('assets/Images/Daylight_Box_0.png');
 
 //spot light
 const spotLight_1 = new SpotLight();
-spotLight_1.setPosition(-3.394, 45.975, 0.449);
+spotLight_1.setPosition(-10.809, 45.975, -3.974);
 spotLight_1.setTarget(cuberoom);
 
 //spot light
@@ -62,10 +61,25 @@ const spotLight_4 = new SpotLight();
 spotLight_4.setPosition(-3.394, 45.975, 0.449);
 spotLight_4.setTarget(image_3);
 
-// listen for an event
-//image_1.addEventListener('click', () => console.log('je klikt op mij'));
+//spot light
+const spotLight_5 = new SpotLight();
+spotLight_5.setPosition(-3.394, -0.534, 0,449);
+spotLight_5.setTarget(melkMeisje);
+
 
 // Event's text
-///image_1.addEventListener('mouseenter', () => document.getElementById('extraText').innerHTML = "Test pop up.");
-/*image_1.addEventListener('click', () =>
-    document.getElementById('extraText').innerHTML = "Test pop up.");*/
+image_1.addEventListener('mouseenter', () =>
+    document.getElementById('extraText').innerHTML = "Kunstwerk ‘Food Chain Project’ van kunstenaar Itamar Gilboa.");
+image_1.addEventListener('mouseleave', () =>
+    document.getElementById('extraText').innerHTML = "");
+image_2.addEventListener('mouseenter', () =>
+    document.getElementById('extraText').innerHTML = "Werkplaats van kunstenaar Folkert de Jong");
+image_2.addEventListener('mouseleave', () =>
+    document.getElementById('extraText').innerHTML = "");
+image_3.addEventListener('mouseenter', () =>
+    document.getElementById('extraText').innerHTML = "Kunstwerk van kunstenaar Sabine Tol");
+image_3.addEventListener('mouseleave', () =>
+    document.getElementById('extraText').innerHTML = "");
+
+melkMeisje.addEventListener('click', () =>
+    document.getElementById('extraText').innerHTML = "Melkmeisje van Vermeer");
